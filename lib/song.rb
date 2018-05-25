33dbdcef6f2fae5c8677f3fd919791d67ab9e17e
Scott Silverman <scottms724@gmail.com>
2018-05-25 18:16:58 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,7 +43,7 @@ class Song
   
   def self.new_from_filename(filename_string)
     filename_array = filename_string.split("-")
-    filename_array.tr()
+    filename_array.tr('.mp3', '')
     
     
   end
