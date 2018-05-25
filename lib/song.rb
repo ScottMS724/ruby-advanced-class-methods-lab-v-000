5f29f080acd64fb85cfb557d2898103363e4a57a
Scott Silverman <scottms724@gmail.com>
2018-05-25 16:42:08 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,6 +36,10 @@ class Song
   def self.find_or_create_by_name(name)
     if @@all.detect { |song| song.name == name }
     else
+    new_song = self.new 
+    new_song.name = song 
+    @@all << new_song 
+    new_song
     
     
   end 
