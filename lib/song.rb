b7d8d9c7a96c741ea480004b5ac7f6cfcbe791f1
Scott Silverman <scottms724@gmail.com>
2018-05-25 18:38:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,6 +48,7 @@ class Song
     new_song_name = filename_array[1]
    
     song = self.new 
+    song.artist_name = new_song_artist_name
     song.name = new_song_name 
     song.artist_name = new_song_artist_name
   end
